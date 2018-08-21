85d1c2c26a2c06482dd6b71cbe7b273caabc9079
issemgane <ali.issemgane@gmail.com>
2018-08-21 13:10:57 +0000

    f cm

---
+++ src/main/java/com/appstude/ServeurGestionStockProduitsApplication.java
@@ -0,0 +1,12 @@
+package com.appstude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
+	}
+}
